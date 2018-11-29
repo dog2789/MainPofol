af84cd79c3b0d6738683626acd40384effa7346a
dog2789 <ds6318@naver.com>
2018-11-29 18:01:17 +0900

    181129-final

--- public/js/about.js
+++ public/js/about.js
@@ -2,18 +2,29 @@ $(".circle1").click(function(){
     $(".skills").css("display", "table");
     $(".strength").css("display", "none");
     $(".who").css("display", "none");
+    $(this).addClass("circle");
+    $(".circle2").removeClass("circle");
+    $(".circle3").removeClass("circle");
 });
 
 $(".circle2").click(function(){
     $(".skills").css("display", "none");
     $(".strength").css("display", "none");
     $(".who").css("display", "block");
+    $(this).addClass("circle");
+    $(".circle1").removeClass("circle");
+    $(".circle1").removeClass("cir-ba");
+    $(".circle3").removeClass("circle");
 });
 
 $(".circle3").click(function(){
     $(".skills").css("display", "none");
     $(".who").css("display", "none");
     $(".strength").css("display", "block");
+    $(this).addClass("circle");
+    $(".circle2").removeClass("circle");
+    $(".circle1").removeClass("cir-ba");
+    $(".circle1").removeClass("circle");
 });
 
 $(".ban-tit").each(function(i){
